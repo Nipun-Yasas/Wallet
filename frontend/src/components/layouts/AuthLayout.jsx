6c321cacaa5c1eccb18becd6d08c79a5c0276564
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-03-16 23:07:55 +0530

    fix the login funtion erros

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import CARD_2 from '../../assets/images/card2.png'
+import { LuTrendingUpDown } from 'react-icons/lu'
 
 export default function AuthLayout({children}) {
   return (
@@ -10,8 +11,39 @@ export default function AuthLayout({children}) {
             {children}
         </div>
 
+        <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
+
+          <div className="grid grid-cols-1 z-10">
+            <StatsInfoCard
+              icon={<LuTrendingUpDown/>}
+              label="Track Your Income and Expenses"
+              value="430,000"
+            />
+          </div>
+
+          <img
+          src={CARD_2}
+          className='w-full h-full object-cover'
+          alt='card'
+          />
+        </div>
+
     
     </div>
 
+  )
+}
+
+const StatsInfoCard = ({icon, label, value}) => {
+  return(
+    <div className="flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-gray-200/50 z-10 ">
+      <div className="w-12 h-12 flex items-center justify-center text-[26px] text-white bg-(color:--primary) rounded-full drop-shadow-xl">
+        {icon}
+      </div>
+      <div>
+        <h6 className='text-xs text-gray-500 mb-1'>{label}</h6>
+        <span className='text-[20px]'>${value}</span>
+      </div>
+    </div>
   )
 }
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,16 +1,73 @@
-import React from 'react'
-import AuthLayout from '../../components/layouts/AuthLayout'
+import React from "react";
 
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import AuthLayout from "../../components/layouts/AuthLayout";
+import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/validateEmail";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("null");
+
+  const navigate = useNavigate();
+
+  const handleLogin= async (e) => {
+    e.preventDefault();
+
+    if(!validateEmail(email)){
+      setError("Please enter a valid email is not valid")
+      return
+    }
+    if(!password){
+      setError("Please enter the password is required")
+      return
+    }
+    setError("");
+  };
+
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-        <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
-        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
           Please enter your details to log in
         </p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email address"
+            placeholder="Enter your email adress"
+            type="text"
+          />
+
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Enter your Password"
+            type="password"
+          />
+
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button type="submit" className="btn-primary mt-4">
+            Login
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account?{" "}
+            <Link to="/signUp" className="font-medium text-(color:--primary) underline">
+              Sign Up
+            </Link>
+
+          </p>
+        </form>
       </div>
     </AuthLayout>
-  )
+  );
 }
