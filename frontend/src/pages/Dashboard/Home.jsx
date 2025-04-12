5ac534266edfbbee65892eb5b1769dd94a31be5c
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-04-12 23:15:53 +0530

    feat: added financial overview section with a piechart

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -8,6 +8,7 @@ import { API_PATHS } from "../../utils/apiPaths";
 
 import InfoCard from "../../components/Cards/InfoCard";
 import RecentTransactions from "../../components/Dashboard/RecentTransactions";
+import FinanceOverview from "../../components/Dashboard/FinanceOverview";
 import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
 import { IoMdCard } from "react-icons/io";
 import { addThousandsSeparator } from "../../utils/helper";
@@ -48,7 +49,7 @@ export default function Home() {
   return (
       <DashboardLayout activeMenu="Dashboard">
         <div className="my-5 mx-auto">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <InfoCard
             icon={<IoMdCard />}
             label="Total Balance"
@@ -69,12 +70,18 @@ export default function Home() {
             value={addThousandsSeparator(dashboardData?.totalExpenses|| 0)}
             color="bg-red-500"
           />
-          </div>
+          </div> */}
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
             <RecentTransactions
             transactions={dashboardData?.recentTransactions}
             onSeeMore={() => navigate("/expense")}
+            />
+
+            <FinanceOverview
+            totalBalance={dashboardData?.totalBalance || 0}
+            totalIncome={dashboardData?.totalIncome || 0}
+            totalExpenses={dashboardData?.totalExpenses || 0}
             />
           </div>
         </div>
