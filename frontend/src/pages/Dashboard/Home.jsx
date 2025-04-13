1887ec6f2f7af84819d6fb831d72dba2f69159d5
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-04-13 20:28:25 +0530

    feat: implemented last60DaysIncome Section

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -11,6 +11,7 @@ import RecentTransactions from "../../components/Dashboard/RecentTransactions";
 import FinanceOverview from "../../components/Dashboard/FinanceOverview";
 import ExpenseTransactions from "../../components/Dashboard/ExpenseTransactions";
 import Last30DaysExpenses from "../../components/Dashboard/Last30DaysExpenses";
+import RecentIncomeWithChart from "../../components/Dashboard/RecentIncomeWithChart";
 import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
 import { IoMdCard } from "react-icons/io";
 import { addThousandsSeparator } from "../../utils/helper";
@@ -95,6 +96,10 @@ export default function Home() {
             data={dashboardData?.last30DaysExpenses?.transactions || []}
             /> */}
 
+            <RecentIncomeWithChart
+            data={dashboardData?.last60DysIncome?.transactions?.slice(0,4) || []}
+            totalIncome={dashboardData?.totalIncome || 0}
+            />
           </div>
         </div>
       </DashboardLayout>
